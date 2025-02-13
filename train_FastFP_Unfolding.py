6d117834f9364a495c64bd77b333e4540d587a9f
zhujhz <87255908+zhujhz@users.noreply.github.com>
2025-02-13 16:11:03 +0800

    Add files via upload

--- train_FastFP_Unfolding.py
+++ train_FastFP_Unfolding.py
@@ -8,107 +8,97 @@ import matplotlib.pyplot as plt
 import time
 from datetime import datetime
 
-#导入自定义函数
+# Import custom functions
 from FP_unfolding.generate_data import split_matrix
 from FP_unfolding.utils import dataset
-from FP_unfolding.FastFP_net_n import FastFPnet
+from FP_unfolding.FastFP_net import FastFPnet
 from FP_unfolding.FastFP_alg import FastFP
 from FP_unfolding.loss_fuction import LossUnfolding
 from FP_unfolding.compute_WSR import WSR
-from FP_unfolding.load_chn import load_H,load_H_BS1
+from FP_unfolding.load_chn import load_H, load_H_BS1
 from FP_unfolding.load_chn import load_H_3GPP
 
 import argparse
 
-
-def train_FPnet(net,epochs,loss_fc,optimizer,lr_scheduler,dataloaders,supervisor_batchs=-1,timestamp=None,record_interval=None,exp_name=None):
+def train_FPnet(net, epochs, loss_fc, optimizer, lr_scheduler, dataloaders, supervisor_batchs=-1, timestamp=None, record_interval=None, exp_name=None):
     
     criterion = loss_fc
 
-    trainloader,valloader = dataloaders
-    time_points,train_epoch_losses,val_epoch_losses =[], [],[]
+    trainloader, valloader = dataloaders
+    time_points, train_epoch_losses, val_epoch_losses = [], [], []
 
     def eval_net():
-
+        """Evaluate the network on the validation set"""
         net.eval()
 
         with torch.no_grad():
             batch_losses = []
-            for batch_idx,H in enumerate(valloader):          
+            for batch_idx, H in enumerate(valloader):          
                 H = H.to(net.device)
-                V,V_out_record = net(H)
-                loss = criterion(H,V,net.weight,mode='unsuper')
+                V, V_out_record = net(H)
+                loss = criterion(H, V, net.weight, mode='unsuper')
                 batch_losses.append(loss.item())
 
-            # print("val loss: ",np.mean(batch_losses))
-            # val_epoch_losses.append(np.mean(batch_losses))
         net.train()
         return np.mean(batch_losses)
     
-
+    # Initialize time and loss tracking
     time_points.append(0)
     val_epoch_losses.append(eval_net())
     train_epoch_losses.append(0)
-    print("Elapsed_time: ",0,"Val loss: ",val_epoch_losses[-1])
+    print("Elapsed_time: ", 0, "Val loss: ", val_epoch_losses[-1])
     start_time = time.time()
-    last_record_time=start_time
+    last_record_time = start_time
 
     batch_losses = []
     for epoch in range(epochs):
-
-        for batch_idx,H in enumerate(trainloader):
-            # print(batch_idx)
+        for batch_idx, H in enumerate(trainloader):
             H = H.to(net.device)
             optimizer.zero_grad()
 
-            V_init=net.init_input(H.size(0))
-            V,V_out_record = net(H,action_noise_power=0,init_V=V_init)
+            V_init = net.init_input(H.size(0))
+            V, V_out_record = net(H, action_noise_power=0, init_V=V_init)
 
-            if supervisor_batchs>=0:
-                supervisor_batchs-=1
-                loss = criterion(H,V,net.weight,V_init=V_init,mode='super',target_iters=40)
+            # Use supervised loss for initial batches, then switch to unsupervised
+            if supervisor_batchs >= 0:
+                supervisor_batchs -= 1
+                loss = criterion(H, V, net.weight, V_init=V_init, mode='super', target_iters=40)
             else:
-                loss = criterion(H,V,net.weight,mode='unsuper')
-                # for V_middle in V_out_record[:-1]:
-                #     loss += (1-discount_factor)*criterion(H,V_middle,net.weight,mode='unsuper')
+                loss = criterion(H, V, net.weight, mode='unsuper')
 
             loss.backward()
             optimizer.step()
             batch_losses.append(loss.item())
 
-            # if time.time() - start_time -record_idx*record_interval>record_interval:
-            #     record_idx+=1
-
-            # 
-            if time.time() - last_record_time>record_interval:
-                last_record_time=time.time()
-                elapsed_time=last_record_time - start_time
+            # Record losses at specified intervals
+            if time.time() - last_record_time > record_interval:
+                last_record_time = time.time()
+                elapsed_time = last_record_time - start_time
                 time_points.append(elapsed_time)
 
                 train_epoch_losses.append(np.mean(batch_losses))
-                print("Elapsed_time: ",elapsed_time,"Train loss: ",train_epoch_losses[-1])
-                batch_losses=[]
+                print("Elapsed_time: ", elapsed_time, "Train loss: ", train_epoch_losses[-1])
+                batch_losses = []
 
                 val_epoch_losses.append(eval_net())
-                print("Elapsed_time: ",elapsed_time,"Val loss: ",val_epoch_losses[-1])
+                print("Elapsed_time: ", elapsed_time, "Val loss: ", val_epoch_losses[-1])
 
+        # lr_scheduler.step()  # Uncomment if you want to use learning rate scheduler
+        print("epoch: ", epoch, " is finished------------------------------------")
 
-        # lr_scheduler.step()
-        print("epoch: ",epoch," is finished------------------------------------")
-
-        # 保存模型
+        # Save model after each epoch
         torch.save(net.state_dict(), f'models\{exp_name}_model_weights_{timestamp}.pth')
 
-        # # 保存训练和验证误差
-        # np.save(f'Loss\Tran_Val_loss_{timestamp}.npy', (np.array(train_epoch_losses,dtype=np.float64),np.array(val_epoch_losses,dtype=np.float64)))
-        np.save(f'Loss\{exp_name}_Time_Tran_Val_loss_{timestamp}.npy', (np.array(time_points,dtype=np.float64),np.array(train_epoch_losses,dtype=np.float64),np.array(val_epoch_losses,dtype=np.float64)))
+        # Save the training and validation errors
+        np.save(f'Loss\{exp_name}_Time_Tran_Val_loss_{timestamp}.npy', (np.array(time_points, dtype=np.float64), np.array(train_epoch_losses, dtype=np.float64), np.array(val_epoch_losses, dtype=np.float64)))
 
-    return time_points,train_epoch_losses,val_epoch_losses
+    return time_points, train_epoch_losses, val_epoch_losses
 
 def main():
+    # Set up argument parser
     parser = argparse.ArgumentParser(description="Parse hyperparameters for the script.")
 
-    # 添加参数
+    # Add arguments
     parser.add_argument("--N_t", type=int, default=64, help="Number of transmit antennas")
     parser.add_argument("--N_r", type=int, default=4, help="Number of receive antennas")
     parser.add_argument("--d", type=int, default=2, help="Number of data streams")
@@ -128,20 +118,18 @@ def main():
     parser.add_argument("--record_interval", type=int, default=120)
     parser.add_argument("--exp_name", type=str, default='')
     
-    # 权重数组需要单独处理
     parser.add_argument("--w", nargs='+', type=float, default=None, help="Weight array")
 
-    # 解析参数
     args = parser.parse_args()
 
-    # 如果未传入权重，则创建默认的等差权重矩阵
+    # Set up weights
     if args.w is None:
-        w = [ 1 for i in range(args.user_number)]
+        w = [1 for i in range(args.user_number)]
         w = [w[i] / sum(w) for i in range(args.user_number)]
     else:
         w = args.w
 
-    # 将参数赋值给变量
+    # Extract arguments
     N_t = args.N_t
     N_r = args.N_r
     d = args.d
@@ -158,67 +146,59 @@ def main():
     net_iterations = args.net_iterations
     hidden_size = args.hidden_size
     single_number = args.single_number
-    record_interval=args.record_interval
+    record_interval = args.record_interval
     exp_name = args.exp_name
 
-
+    # Define learning rate schedule
     def lr_lambda(epoch):
         if epoch < 100:
-            return 1.0  # 保持初始学习率
+            return 1.0  # Maintain the initial learning rate
         else:
-            return unsupervisor_lr / supervisor_lr  # 调整到目标学习率
+            return unsupervisor_lr / supervisor_lr  # Adjust to the target learning rate.
 
-
+    # Set device (GPU if available, else CPU)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using: ",device)
+    print("using: ", device)
 
-    Data_blocks=N_samples//single_number
-    H=[]
+    # Load and prepare data
+    Data_blocks = N_samples // single_number
+    H = []
     for i in range(Data_blocks):
-        if BS_number>1:
+        if BS_number > 1:
             H.append(load_H(f"Data/data/{i+1}_{single_number}_Nr_{N_r}_Nt_{N_t}_N_user_{user_number}_BS_{BS_number}.mat"))
         else:
             H.append(load_H_BS1(f"Data/data/{i+1}_{single_number}_Nr_{N_r}_Nt_{N_t}_N_user_{user_number}_BS_{BS_number}.mat"))
-    H=torch.cat(H,dim=0)
-    # 分割数据
-    train_set, val_set, test_set = split_matrix(H=H,train_size=0.8)
+    H = torch.cat(H, dim=0)
+    
+    # Split the dataset
+    train_set, val_set, test_set = split_matrix(H=H, train_size=0.8)
 
-
-    # 创建DataLoder
+    # Prepare DataLoaders
     trainset = dataset(train_set)
-    trainloader = torch.utils.data.DataLoader(trainset,batch_size = batch_size)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size)
 
     valset = dataset(val_set[:500])
-    valloader = torch.utils.data.DataLoader(valset,batch_size = batch_size)
-    dataloaders = [trainloader,valloader]
+    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size)
+    dataloaders = [trainloader, valloader]
 
-
-
-    net = FastFPnet(BS_number=BS_number,user_number=user_number,N_t=N_t,N_r=N_r,w=w,d=d,iterations=net_iterations,device=device,hidden_size=hidden_size,signal_max_power=signal_max_power,noise_power=noise_power)
-
-    optimizer = optim.Adam(net.parameters(), lr = supervisor_lr , weight_decay = 0)
-
+    # Initialize network, optimizer, scheduler, and loss function
+    net = FastFPnet(BS_number=BS_number, user_number=user_number, N_t=N_t, N_r=N_r, w=w, d=d, iterations=net_iterations, device=device, hidden_size=hidden_size, signal_max_power=signal_max_power, noise_power=noise_power)
+    optimizer = optim.Adam(net.parameters(), lr=supervisor_lr, weight_decay=0)
     scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
-    loss_fuc=LossUnfolding(signal_max_power=signal_max_power,noise_power=noise_power)
+    loss_fuc = LossUnfolding(signal_max_power=signal_max_power, noise_power=noise_power)
 
-
-    # 训练并记录时间
-    start_time=time.time()
+    # Training
+    start_time = time.time()
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    supervisor_batchs=int(supervisor_epoch_num*train_set.size(0)/batch_size)
-    record_time,train_loss,val_loss= train_FPnet(net=net,epochs=epochs,loss_fc=loss_fuc,optimizer=optimizer,lr_scheduler=scheduler,dataloaders=dataloaders,supervisor_batchs=supervisor_batchs,timestamp=timestamp,record_interval=record_interval,exp_name=exp_name)
+    supervisor_batchs = int(supervisor_epoch_num * train_set.size(0) / batch_size)
+    record_time, train_loss, val_loss = train_FPnet(net=net, epochs=epochs, loss_fc=loss_fuc, optimizer=optimizer, lr_scheduler=scheduler, dataloaders=dataloaders, supervisor_batchs=supervisor_batchs, timestamp=timestamp, record_interval=record_interval, exp_name=exp_name)
     end_time = time.time()
-    # 计算运行时间
     elapsed_time = end_time - start_time
     print(f"The net took {elapsed_time} seconds to train.")
 
-    # 获取当前时间并格式化为字符串
-    # timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    # 保存模型参数
+    # Save model and losses
     torch.save(net.state_dict(), f'models\{exp_name}_model_weights_{timestamp}.pth')
-    # 保存训练和验证误差
-    np.save(f'Loss\{exp_name}_Time_Tran_Val_loss_{timestamp}.npy', (np.array(record_time,dtype=np.float64),np.array(train_loss,dtype=np.float64),np.array(val_loss,dtype=np.float64)))
-
+    np.save(f'Loss\{exp_name}_Time_Tran_Val_loss_{timestamp}.npy', (np.array(record_time, dtype=np.float64), np.array(train_loss, dtype=np.float64), np.array(val_loss, dtype=np.float64)))
 
 if __name__ == "__main__":
     main()
